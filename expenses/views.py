8ea0db509d8bacc74326ef92b153e784e97b2a6d
Badal Shrestha <shresthabadal324@gmail.com>
2026-06-06 12:50:38 +0545

    added view to handle main dashboard

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Expense
+from .serializers import ExpenseSerializers
 
 # Create your views here.
+def dashboard(request):
+    return render(request, 'expenses/dashboard.html')
