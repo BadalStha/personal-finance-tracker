743b738196546b1414267fbf8ad85e5ce4ed0482
Badal Shrestha <shresthabadal324@gmail.com>
2026-06-06 14:14:11 +0545

    fixed a error where the data was saving both date and time

--- expenses/serializers.py
+++ expenses/serializers.py
@@ -3,6 +3,7 @@ from .models import Expense
 
 class ExpenseSerializer(serializers.ModelSerializer):
     category_display = serializers.CharField(source='get_category_display', read_only=True)
+    date = serializers.DateField(format="%Y-%m-%d")
 
     class Meta:
         model = Expense
